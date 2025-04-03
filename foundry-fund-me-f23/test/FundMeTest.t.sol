a8a4f77300fe2cd46a6a70b716bcbf32691e191c
zhangjie <beimian.zj@raycloud.com>
2025-04-03 12:07:38 +0800

    refactoring

--- foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -4,14 +4,27 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from 'forge-std/Test.sol';
 import {FundMe} from '../src/FundMe.sol';
+import {DeployFundMe} from '../script/DeployFundMe.s.sol';
 
 contract FundMeTest is Test {
     FundMe fundme;
     function setUp() external { 
-        fundme = new FundMe();
+        DeployFundMe deployer = new DeployFundMe();
+        fundme = deployer.run();
     }
 
     function testMinimumDollarIsFive() public view {
-        assertEq(fundme.MINIMUM_USD(), 6e18);
+        assertEq(fundme.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        console.log(fundme.i_owner());
+        console.log(msg.sender);
+        assertEq(fundme.i_owner(), address(this));
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundme.getVersion();
+        assertEq(version, 4);
     }
 }
