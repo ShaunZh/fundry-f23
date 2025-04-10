0f7ae84856868bb08d7d0c604e0a92351da5a4a6
zhangjie <beimian.zj@raycloud.com>
2025-04-10 19:59:54 +0800

    Fundry Fundamentals: 20. Create integration tests

--- foundry-fund-me-f23/test/uint/FundMeTest.t.sol
+++ foundry-fund-me-f23/test/uint/FundMeTest.t.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from 'forge-std/Test.sol';
-import {FundMe} from '../src/FundMe.sol';
-import {DeployFundMe} from '../script/DeployFundMe.s.sol';
+import {FundMe} from '../../src/FundMe.sol';
+import {DeployFundMe} from '../../script/DeployFundMe.s.sol';
 
 uint256 constant GAS_PRICE = 1;
 
